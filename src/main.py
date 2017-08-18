23a446d7c8f930939186a65525958f6ecf3e0ee1
Paul Fenton <paul.fenton1988@gmail.com>
2017-08-17 20:38:54 -0500

    first attempt

--- src/main.py
+++ src/main.py
@@ -8,14 +8,19 @@ from sklearn.ensemble.forest import RandomForestRegressor
 from numpy import float32
 from sklearn.ensemble.gradient_boosting import GradientBoostingRegressor
 
+# load the data
 test_dataset=pd.read_csv('../competition/test.csv')
 train_dataset=pd.read_csv('../competition/train.csv')
+
+# designate the dataset subsets
 X_train=train_dataset.iloc[:,1:-1].values
 Y_train=train_dataset.iloc[:,80:81].values
 X_test=test_dataset.iloc[:,1:].values
 X=pd.concat([DataFrame(X_train),DataFrame(X_test)])
 
 X=X.iloc[:,:].values
+
+# remove missing values... ? what do the last 3 lines do?
 from sklearn.preprocessing import Imputer
 imputer=Imputer(missing_values='NaN',strategy='mean',axis=0)
 imputer.fit(X_train[:,2:3])
@@ -36,6 +41,7 @@ X_train=X_train.iloc[:,:].values
 X_test=X_test.iloc[:,:].values
 X=X.iloc[:,:].values
 
+# turn the categorical variables into one-hot vectors
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 l=[1,4,5,6,7,8,9,10,11,12,13,14,15,18,19,20,21,22,23,24,26,27,28,29,30,31,32,34,38,39,40,41,52,54,56,57,58,59,62,63,64,71,72,73,76,77,78]
 for i in l:
